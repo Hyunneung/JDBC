cd6a6e7d7366df5322d3e25fab07f5c2bc39b49a
Hyunneung <neong_@naver.com>
2022-06-23 22:23:16 +0900

    statement

---
+++ statement/arraylist_동적쿼리1/DAO.java
@@ -0,0 +1,84 @@
+// // 데이터베이스와 연동해 데이터베이스의 테이블 내용을 읽는 프로그램
+package statement.arraylist_동적쿼리1;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class DAO {
+	public ArrayList<Emp> search(String[] searchword) {
+		ArrayList<Emp> emplist = null;
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String select_sql = "select * from emp ";
+		String menus[] = { "empno", "ename", "job", "mgr", "hiredate", "sal", "comm", "deptno" };
+
+		// sql문 작성
+		for (int i = 0; i < searchword.length; i++) {
+			if (searchword[i] != null) {
+				String single = "";
+				if (i == 1 || i== 2 || i== 4)
+					single = "'";
+				if (select_sql.contains("where")) {
+					select_sql += " and ";
+				} else {
+					select_sql += "where ";
+				}
+				select_sql += menus[i] + " = " + single + searchword[i].toUpperCase() + single;
+			}
+		} // for 끝
+
+		System.out.println(select_sql + "\n");
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
+			stmt = conn.createStatement();
+
+			rs = stmt.executeQuery(select_sql);
+
+			int i = 0;
+			while (rs.next()) {
+				if (i++ == 0) { // emplist는 데이터가 있을 때 1번만 만든다
+					emplist = new ArrayList<Emp>();
+				}
+
+				Emp e = new Emp(); // 사원마다 객체 각각 생성
+				e.setEmpno(rs.getInt(1));
+				e.setEname(rs.getString(2));
+				e.setJob(rs.getString(3));
+				e.setMgr(rs.getInt(4));
+				e.setHiredate(rs.getDate(5));
+				e.setSal(rs.getInt(6));
+				e.setDeptno(rs.getInt(7));
+
+				emplist.add(e);
+			} // while 끝
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("해당 클래스를 찾을 수 없습니다." + cnfe.getMessage());
+		} catch (SQLException se) {
+			System.out.println(se.getMessage());
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (SQLException se) {
+				System.out.println(se.getMessage());
+			}
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se) {
+				System.out.println(se.getMessage());
+			}
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		} // try-catch-finally 끝
+
+		return emplist;
+	}
+}
