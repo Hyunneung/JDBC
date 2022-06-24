1d49db7b0370aedff221813c77cf0f64a094f2dc
Hyunneung <neong_@naver.com>
2022-06-25 02:32:16 +0900

    preparedstatement

---
+++ preparedstatement/Emp.java
@@ -0,0 +1,86 @@
+// emp 테이블
+package preparedstatement;
+
+import java.sql.Date;
+
+// DTO (Data Transfer Object)
+public class Emp {
+	// 데이터베이스에 있는 테이블의 컬럼명을 똑같이 작성한다
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private Date hiredate;
+	private int sal;
+	private int comm;
+	private int deptno;
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public int getMgr() {
+		return mgr;
+	}
+
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+
+	public Date getHiredate() {
+		return hiredate;
+	}
+
+	public void setHiredate(Date hiredate) {
+		this.hiredate = hiredate;
+	}
+
+	public int getSal() {
+		return sal;
+	}
+
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+
+	public int getComm() {
+		return comm;
+	}
+
+	public void setComm(int comm) {
+		this.comm = comm;
+	}
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%-8s%-8s%-16s%s\t%-16s%s\t%s\t%s\t", empno, ename, job, mgr, hiredate, sal, comm, deptno);
+	}
+}
